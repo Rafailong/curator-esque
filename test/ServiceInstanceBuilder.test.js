a6480b3109d05271934b0d441849586f507e19ba
Rafael Avila <rafa.avim@gmail.com>
2016-04-12 11:04:31 -0500

    add payload to ServiceInstance

--- test/ServiceInstanceBuilder.test.js
+++ test/ServiceInstanceBuilder.test.js
@@ -15,10 +15,19 @@ describe('ServiceInstanceBuilder', function () {
 
   it('should build an instance of ServiceInstance when calling build() with parameters', function () {
     builder.name('my/service/name')
+    builder.address('127.0.0.1')
+    builder.port('8080')
+    var payload = {
+      'something': 'lol'
+    }
+    builder.payload(payload)
 
     var serviceInstance = builder.build()
 
     serviceInstance.should.be.a('object')
     serviceInstance.name.should.equal('my/service/name')
+    serviceInstance.address.should.equal('127.0.0.1')
+    serviceInstance.port.should.equal('8080')
+    serviceInstance.payload.should.equal(payload)
   })
 })
